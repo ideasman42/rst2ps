34f24e9e031bb0f126d3033afc3d5b4dc1130c10
Campbell Barton <ideasman42@gmail.com>
2013-12-15 01:16:31 +1100

    working image support

--- rst2ps.py
+++ rst2ps.py
@@ -7,12 +7,12 @@
 #
 # b -b /src/rst2ps/tests/camera.blend --python /src/rst2ps/rst2ps.py
 
-def ps_from_poly(fw, points, rgb=(0.0, 0.0, 0.0)):
+def ps_from_poly(fw, points, color=(0.0, 0.0, 0.0)):
     fw("newpath\n")
     for i, p in enumerate(points):
         fw("%.6f %.6f %s\n" % (p[0], p[1], "moveto" if i == 0 else "lineto"))
     fw("closepath\n")
-    fw("%.4f %.4f %.4f setrgbcolor\n" % rgb)
+    fw("%.4f %.4f %.4f setrgbcolor\n" % color)
     fw("fill\n")
 
 
@@ -53,7 +53,7 @@ def ps_from_obj_curve(fw, obj, matrix):
         fw("fill\n")
 
 
-def ps_from_obj_image(fw, obj, matrix):
+def ps_from_obj_image(fw, obj, matrix, use_placeholder=False):
     # Seems this is ghostscript specific
     # requires '-dNOSAFER' arg.
     import bpy
@@ -63,53 +63,69 @@ def ps_from_obj_image(fw, obj, matrix):
 
     if image is None:
         return
+
+    is_missing = False
+
     filepath = bpy.path.abspath(image.filepath, library=image.library)
 
     if not os.path.exists(filepath):
         print("  image path missing: %r -> %r" % (obj.name, filepath))
+        is_missing = True
 
     x, y = image.size
+    x = max(1, x)
+    y = max(1, y)
 
     if x < y:
         aspx, aspy = x / y, 1.0
     else:
         aspx, aspy = 1.0, y / x
 
-    if 1:
-        from mathutils import Vector
 
-        points = [Vector() for i in range(4)]
-        dim = obj.empty_draw_size
-        ofs = obj.empty_image_offset[:]
-        points[0].xy = 0.0, 0.0
-        points[3].xy = 0.0, dim
-        points[2].xy = dim, dim
-        points[1].xy = dim, 0.0
+    from math import degrees
+    from mathutils import Vector
 
-        for p in points:
-            p.x += dim * ofs[0]
-            p.y += dim * ofs[1]
+    points = [Vector() for i in range(4)]
+    dim = obj.empty_draw_size
+    ofs = [f * dim for f in obj.empty_image_offset]
 
-            p.x *= aspx
-            p.y *= aspy
+    points[0].xy = 0.0, 0.0
+    points[1].xy = 0.0, dim
+    points[2].xy = dim, dim
+    points[3].xy = dim, 0.0
 
-        points = [matrix * p for p in points]
+    for p in points:
+        p.x = (p.x + ofs[0]) * aspx
+        p.y = (p.y + ofs[1]) * aspy
 
-        ps_from_poly(fw, points)
+    points = [matrix * p for p in points]
+
+    if use_placeholder:
+        ps_from_poly(fw, points, color=(0.0, 0.0, 0.0))
+    elif is_missing:
+        ps_from_poly(fw, points, color=(1.0, 0.0, 1.0))
     else:
+        # place image based on 'points' quad vectors.
         fw("gsave\n")
 
-        # fw("0 0 translate\n")
-        # fw("10 10 scale\n")
+        fw("%.6f %.6f translate\n" % (points[1][0], points[1][1]))
+        fw("%.6f rotate\n" % degrees((points[0] - points[1]).xy.angle_signed(Vector((0.0, -1.0)))))
+
+        dim_x = (points[0].xy - points[3].xy).length
+        dim_y = (points[0].xy - points[1].xy).length
+
+        mtx_x = x / dim_x
+        mtx_y = y / dim_y
 
         fw("%d %d "  # size of image
            "8 "  # bits per channel (1, 2, 4, or 8)
-           "[1 0 0 -1 0 1] "  # transform array... maps unit square to pixel
+           "[%.6f 0 0 %.6f 0 %.6f] "  # transform array... maps unit square to pixel
            "(%s) (r) file/DCTDecode filter "  # opens the file and filters the image data
            "false "  # _don't_ pull channels from separate sources.
            "3 "  # channels
-           "colorimage\n" % (size[0], size[1], filepath))
-
+           "colorimage\n" % (x, y,
+                             mtx_x, -mtx_y, mtx_y,  # matrix values
+                             filepath))
         fw("grestore\n")
 
 
@@ -204,11 +220,14 @@ def ps_write(fw):
 
     fw("showpage\n")
 
+
 f = open("/test.ps", "w")
 ps_write(f.write)
 f.close()
 
-import os
+# import os
 # -dNOSAFER for images
 # os.system("ps2pdf -dNOSAFER -dEPSCrop -dAutoRotatePages=/None -dAutoFilterColorImages=false -dColorImageFilter=/FlateEncode -dUseFlateCompression=true /test.ps /test.pdf")
 # gv  -nosafer /test.ps
+#
+# b -b /src/rst2ps/tests/camera.blend --python /src/rst2ps/rst2ps.py ; gv -nosafer /test.ps
